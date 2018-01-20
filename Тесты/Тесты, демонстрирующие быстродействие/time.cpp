ae56a303a35a328814b494a4a0d2aba2409a3219
Alexander <32273263+paniker228@users.noreply.github.com>
2018-01-20 16:08:33 +0300

    TESTS_UPDATE

--- Тесты/Тесты, демонстрирующие быстродействие/time.cpp
+++ Тесты/Тесты, демонстрирующие быстродействие/time.cpp
@@ -31,10 +31,10 @@ bool check(string s)
 
 int main()
 {
-
+    srand(time(0));
     setlocale(LC_ALL, "Rus");
     heap *h = new heap();
-    int m = 1000000;
+    int m = 1000000, move;
     vector<heap*> heaps(m, new heap());
 
     ifstream in("input.txt");
@@ -43,26 +43,44 @@ int main()
     string s, s1, s2;
     int n;
 
-        in >> s;
-
-        if (!check(s)) ERROR;
-        n = atoi(s.c_str());
-
-        if (n <= 0) ERROR;
-        if (n > 100) ERROR;
-        for (int i = 0; i < m; i++)
+    in >> s;
+    if (s == "INSERT")
+    {
+        move = 0;
+    } else
+        if (s == "EXTRACT")
         {
-            int k = rand();
-            for (int j = 0; j < n; j++)
-            {
-                heaps[i]->insert(k);
-            }
+            move = 1;
         }
+        else ERROR;
 
-        long long time = clock();
-        for (int i = 0; i < m; i++) heaps[i]->insert(rand());
-        out << clock() - time;
-    DA_ETO_METKA:
-        in.close();
-        out.close();
+    in >> s;
+    if (!check(s)) ERROR;
+    n = atoi(s.c_str());
+
+    if (n <= 0) ERROR;
+    if (n > 100) ERROR;
+    for (int i = 0; i < m; i++)
+    {
+        int k = rand();
+        for (int j = 0; j < n; j++)
+        {
+            heaps[i]->insert(k);
+        }
+    }
+
+    long long time = clock();
+    if (move == 0)
+    {
+        int k = rand();
+        for (int i = 0; i < m; i++) heaps[i]->insert(k);
+    }
+    else
+    {
+        for (int i = 0; i < m; i++) heaps[i]->extract_min();
+    }
+    out << (clock() - time);
+DA_ETO_METKA:
+    in.close();
+    out.close();
 }
